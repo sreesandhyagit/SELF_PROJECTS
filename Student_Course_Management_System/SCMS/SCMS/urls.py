5d4e773b6948ff583384c238ed980b3b8049b25b
sreesandhyagit <sreesandhyaajesh@gmail.com>
2025-12-15 19:28:57 +0530

    error

--- Student_Course_Management_System/SCMS/SCMS/urls.py
+++ Student_Course_Management_System/SCMS/SCMS/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for SCMS project.
+URL configuration for scms project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
--- Student_Course_Management_System/SCMS/manager/views.py
+++ Student_Course_Management_System/SCMS/manager/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 def managerDashboard(request):
     return render(request,"manager-dashboard.html")
